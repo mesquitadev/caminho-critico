3cefe2a9c1769a11617e1af289fc16f62fb25ebc
F8176976 Paulo Roberto de Rezende <prezende@bb.com.br>
2022-12-05 14:15:38 -0300

    aumento time keep alive

--- sgs_caminho_critico/antecessores.py
+++ sgs_caminho_critico/antecessores.py
@@ -1,19 +1,5 @@
 from graph_generator import build_edges, draw_graph
-from utils import read_csv_file, remove_list_duplicate
-
-itens_entrada = []
-itens_saida = []
-no_origem = []
-entrada = read_csv_file('br_in.csv', ',')
-saida = read_csv_file('br_out.csv', ',')
-
-
-def get_itens_saida_ant():
-    return itens_saida
-
-
-def get_itens_entrada_ant():
-    return itens_entrada
+from utils import remove_list_duplicate
 
 
 def get_item_by_node_ant(node_label, list_name, i, tipo='+'):
@@ -25,8 +11,7 @@ def get_item_by_node_ant(node_label, list_name, i, tipo='+'):
     return itens
 
 
-def monta_grafo_ant(no_origem, edges, no_destino, mapa):  # no_origem
-    print(edges, no_destino)
+def monta_grafo_ant(no_origem, edges, no_destino, mapa, entrada, saida, all_edges, no_origem_anterior):
     if no_origem:
         no_origem = get_item_by_node_ant(no_origem, entrada, 0)  # itens_entrada  # entrada  # 0
     else:
@@ -36,10 +21,11 @@ def monta_grafo_ant(no_origem, edges, no_destino, mapa):  # no_origem
         cond_inicial = v[1]  # captura condições para busca no outro arquivo
         no_destino = get_item_by_node_ant(cond_inicial, saida, 1)  # no_origem # saida
         edges = build_edges(no_origem[0][0], no_destino)
+        all_edges.append(edges)
         mapa.add_edges_from(edges)
         edges = []
-        draw_graph(root_node_label=no_origem, grafo=mapa, node_size=2000, font_size=8,
-                   origin_color='olive', others_color='blue', font_color='whitesmoke')
+        # draw_graph(root_node_label=no_origem, grafo=mapa, node_size=2000, font_size=8,
+        #            origin_color='olive', others_color='blue', font_color='whitesmoke')
         if no_destino and no_destino[0][0] in no_origem[0][0]:
             continue
             # no_origem_anterior.append(no_destino[0])
@@ -62,7 +48,8 @@ def monta_grafo_ant(no_origem, edges, no_destino, mapa):  # no_origem
         # draw_graph(mapa, node_size=2000, font_size=8)
 
         for no in no_destino:
-            monta_grafo_ant(no, edges, no_destino, mapa)  # no_origem
+            monta_grafo_ant(no, edges, no_destino, mapa, entrada, saida, all_edges, no_origem_anterior)
+            # monta_grafo_ant(no, edges, no_destino, mapa)  # no_origem
 
 # rodadas de sucesso:
 # ACLD137,ACLD200,ACLDH07,ACLD250,ACLD775,DTII010,
--- sgs_caminho_critico/run.py
+++ sgs_caminho_critico/run.py
@@ -79,23 +79,3 @@ def fetch_graph_data(rotina=None, grupo=None, tipo=1, ambiente='br'):
         return jsonify_nodes_edges(all_edges)
     else:
         return None
-
-
-def fetch_graph_data_old():
-    nodes = [{"id": "1", "title": "Service1", "subTitle": "instance:#2", "detail__role": "load",
-              "arc__failed": 0.7, "arc__passed": 0.3, "mainStat": "qaz"},
-             {"id": "2", "title": "Service2", "subTitle": "instance:#2", "detail__role": "transform",
-              "arc__failed": 0.5, "arc__passed": 0.5, "mainStat": "qaz"},
-             {"id": "3", "title": "Service3", "subTitle": "instance:#3", "detail__role": "extract",
-              "arc__failed": 0.3, "arc__passed": 0.7, "mainStat": "qaz"},
-             {"id": "4", "title": "Service3", "subTitle": "instance:#1", "detail__role": "transform",
-              "arc__failed": 0.5, "arc__passed": 0.5, "mainStat": "qaz"},
-             {"id": "5", "title": "Service4", "subTitle": "instance:#5", "detail__role": "transform",
-              "arc__failed": 0.5, "arc__passed": 0.5, "mainStat": "qaz"}]
-    edges = [{"id": "1", "source": "1", "target": "2", "mainStat": 53},
-             {"id": "2", "source": "2", "target": "3", "mainStat": 53},
-             {"id": "2", "source": "1", "target": "4", "mainStat": 5},
-             {"id": "3", "source": "3", "target": "5", "mainStat": 70},
-             {"id": "4", "source": "2", "target": "5", "mainStat": 100}]
-    result = {"edges": edges, "nodes": nodes}
-    return result
