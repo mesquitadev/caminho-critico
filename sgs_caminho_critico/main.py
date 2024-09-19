965dfedb8c8be31583efca110329f6cc0ef40ea5
C1333806 Paulo Victor Costa Mesquita Pinto <c1333806@interno.bb.com.br>
2024-09-19 15:29:26 -0300

    chore: limpando codigo nao usado

--- sgs_caminho_critico/main.py
+++ sgs_caminho_critico/main.py
@@ -1,56 +1,173 @@
+import threading
+import time
 import networkx as nx
+import csv
+import psycopg2
+import json
+from psycopg2.extras import RealDictCursor
 
-from sgs_caminho_critico.descendentes import monta_grafo
-from sgs_caminho_critico.antecessores import monta_grafo_ant
-from sgs_caminho_critico.utils import is_char_a2z, jsonify_nodes_edges, read_csv_file
-from sgs_caminho_critico.graph_generator import draw_graph
 
-# rodadas de sucesso:
-# ACLD137,ACLD200,ACLDH07,ACLD250,ACLD775,DTII010,ACLD847A
+class PostgresRepository:
+    def __init__(self, dbname, user, password, host, port):
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def connect(self):
+        self.connection = psycopg2.connect(
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port
+        )
+
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+
+    def fetch_nodes_data(self, node_ids):
+        with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            query = """
+            select
+            'stopwatch' as icon, '#377' as color, 'wait' as mainstat,
+            sa.idfr_sch as id, sa.nm_mbr as title, sa.sub_apl as subtitle,
+             sa.pas_pai as detail__pasta, sa.nm_svdr as detail__amb
+            from batch.sch_agdd sa where idfr_sch in %s
+            """
+            cursor.execute(query, (tuple(node_ids),))
+            return cursor.fetchall()
+
+
+def read_csv_file(file_name, delimiter=','):
+    records = []
+    with open(file_name, 'r') as file:
+        csvreader = csv.DictReader(file, delimiter=delimiter)
+        for row in csvreader:
+            records.append((row['idfr_sch'], row['idfr_job_exct']))
+    return records
+
+
+def worker(grafo, records, start, end):
+    for i in range(start, end):
+        origem, destino = records[i]
+        grafo.add_edge(origem, destino)
+
+
+def construir_grafo(records, num_threads=4):
+    print('Construindo grafo...')
+    start_time = time.time()
+
+    grafo = nx.DiGraph()
+    threads = []
+    chunk_size = len(records) // num_threads
+
+    for i in range(num_threads):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size if i != num_threads - 1 else len(records)
+        thread = threading.Thread(target=worker, args=(grafo, records, start, end))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    end_time = time.time()
+    processing_time = end_time - start_time
+    print(f"Tempo de processamento: {processing_time:.2f} segundos")
+
+    return grafo
+
+
+def show_loading_message():
+    while getattr(threading.current_thread(), "do_run", True):
+        print("Carregando...", end="\r")
+        time.sleep(0.5)
+
+
+def encontrar_caminho(grafo, rotina_inicial, rotina_destino):
+    loading_thread = threading.Thread(target=show_loading_message)
+    loading_thread.start()
+    try:
+        caminhos = list(nx.all_simple_paths(grafo, source=rotina_inicial, target=rotina_destino, cutoff=10))
+        return caminhos
+    except nx.NetworkXNoPath:
+        return None
+    finally:
+        loading_thread.do_run = False
+        loading_thread.join()
+
+
+def exibir_edges(caminho):
+    edges = []
+    for i in range(len(caminho) - 1):
+        origem = caminho[i]
+        destino = caminho[i + 1]
+        edges.append((origem, destino))
+    return edges
+
+
+def get_node_color(grafo, root_node, origin_color, others_color):
+    colors = []
+    for node in grafo:
+        if node == root_node:
+            colors.append(origin_color)
+        else:
+            colors.append(others_color)
+    return colors
+
+
+def remover_repetidos(caminhos):
+    caminhos_unicos = set()
+    for caminho in caminhos:
+        caminhos_unicos.add(tuple(caminho))
+    return [list(caminho) for caminho in caminhos_unicos]
+
+
+def main(file_name, rotina_inicial, rotina_destino, db_config, output_file):
+    records = read_csv_file(file_name)
+    grafo = construir_grafo(records)
+    caminhos = encontrar_caminho(grafo, rotina_inicial, rotina_destino)
+    if caminhos:
+        caminhos_unicos = remover_repetidos(caminhos)
+        nodes = set()
+        edges = []
+        for caminho in caminhos_unicos:
+            nodes.update(caminho)
+            edges.extend(exibir_edges(caminho))
+        nodes = list(nodes)
+
+        # Conectar ao banco de dados e buscar dados dos nós
+        repo = PostgresRepository(**db_config)
+        repo.connect()
+        nodes_data = repo.fetch_nodes_data(nodes)
+        repo.disconnect()
+
+        result = {'nodes': nodes_data, 'edges': [{'source': origem, 'target': destino} for origem, destino in edges]}
+    else:
+        result = {'nodes': [], 'edges': []}
+
+    # Salvar o resultado em um arquivo JSON
+    with open(output_file, 'w') as json_file:
+        json.dump(result, json_file, indent=4)
+
+    return result
+
 
 if __name__ == '__main__':
-    edges = []
-    all_edges = []
-    no_origem_anterior = []
-    itens_entrada = []
-    no_destino = ""
-    no_inicial = input('Digite o no inicial: ').upper()
-    grupo = input('Informe o grupo da schedule, em caso de prévias, ou apenas tecle enter ').upper()
-    tipo_grafo = input('Tipo do grafo [1 - sucessores, 2 - antecessores] ')
-    ambiente = input('Digite o plex: [br, b2, b3, hm] ').lower()
-    entrada = read_csv_file('br_in.csv', ',')
-    saida = read_csv_file('br_out.csv', ',')
-
-    if grupo and is_char_a2z(grupo[0]) is None:
-        grupo = grupo[1:]
-
-    if tipo_grafo == '1':  # mapa com sucessores
-        mapa = nx.Graph()
-        monta_grafo(no_inicial, grupo, edges, itens_entrada, mapa, entrada, saida, all_edges, no_origem_anterior)
-        edges_gerados = all_edges  # get_edges()
-        print(f'{jsonify_nodes_edges(all_edges)}')  # get_edges()
-
-        if edges_gerados:
-            print(f'edges: {edges_gerados}')
-            # edges_to_db, nodes_to_db = get_graph_table_records_from_list(edges_gerados)
-            # edges_to_db, nodes_to_db = get_insert_sql_from_list(edges_gerados)
-            draw_graph(root_node_label=no_inicial, grafo=mapa, node_size=2000, font_size=8,
-                       origin_color='olive', others_color='blue', font_color='whitesmoke')
-        else:
-            print(f'A rotina {no_inicial} não passa condição para outras rotinas')
-
-    else:  # mapa com antecessores
-        if grupo and is_char_a2z(grupo[0]) is None:
-            grupo = grupo[1:]
-
-        mapa = nx.Graph()
-        monta_grafo_ant(no_inicial, edges, no_destino, mapa, entrada, saida, all_edges, no_origem_anterior)
-
-        # gerar executavel
-        edges_gerados = all_edges  # get_edges()
-        if edges_gerados:
-            print(f'edges: {edges_gerados}')
-            draw_graph(root_node_label=no_inicial, grafo=mapa, node_size=2000, font_size=8,
-                       origin_color='olive', others_color='blue', font_color='whitesmoke')
-        else:
-            print(f'A rotina {no_inicial} não passa condição para outras rotinas')
+    file_name = 'edges_novo_cp.csv'
+    rotina_inicial = '67958'
+    rotina_destino = '69884'
+    db_config = {
+        'dbname': 'pcp',
+        'user': 'user_gprom63',
+        'password': 'magic123',
+        'host': 'silo01.postgresql.bdh.desenv.bb.com.br',
+        'port': '5432'
+    }
+    output_file = 'result.json'
+    result = main(file_name, rotina_inicial, rotina_destino, db_config, output_file)
+    print(result)
